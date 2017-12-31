27be1f226130d521763e737048740384959144fb
Frankie <frankieprimerano@gmail.com>
2017-12-31 04:27:33 -0500

    Added colors

--- cryptotracker.py
+++ cryptotracker.py
@@ -16,6 +16,16 @@ parser.add_argument("-rm", "--remove", help="Currency to remove from portfolio")
 parser.add_argument("-p", "--portfolio", help="View portfolio", action="store_true")
 args = parser.parse_args()
 
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 class CoinTracker:
     def __init__(self):
         self.currencies_supported = ["AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PKR", "PLN", "RUB", "SEK", "SGD", "THB", "TRY", "TWD", "ZAR"]
@@ -43,31 +53,40 @@ class CoinTracker:
 
     def getCoinData(self, coins):
         coin_data = self.getAPIData()
-        print("Name\tPrice ({0})\tMarket Cap ({0})\tPercent Change\n".format(self.currency))
+        print(colors.WARNING + colors.BOLD + "Name\tPrice ({0})\tMarket Cap ({0})\tPercent Change\n".format(self.currency) + colors.ENDC)
         for coin in coins:
             for c in coin_data:
                 if coin.upper() == c['symbol'] or coin.lower() == c['name'].lower():
                     name = c['symbol']
-                    price = c['price_' + self.currency.lower()]
+                    price = colors.HEADER + c['price_' + self.currency.lower()] + colors.ENDC
                     market_cap = c['market_cap_' + self.currency.lower()]
                     percent_change = c['percent_change_1h']
-
-                    print("{}\t{}\t\t{}\t\t{}%\n".format(name, price, market_cap, percent_change))
+	            if percent_change.startswith("-"):
+                        percent_change = colors.FAIL + percent_change + "%" + colors.ENDC
+                    else:
+                        percent_change = colors.OKGREEN + percent_change + "%" + colors.ENDC
+                    print("{}\t{}\t\t{}\t\t{}\n".format(name, price, market_cap, percent_change))
 
     def getPortfolioData(self):
         coin_data = self.getAPIData()
         portfolio = self.getPortfolio()
-        print("Name\tPrice ({0})\tMarket Cap ({0})\tPercent Change\tHoldings\tValue ({0})\n".format(self.currency))
+        print(colors.WARNING + colors.BOLD + "Name\tPrice ({0})\tMarket Cap ({0})\tPercent Change\tHoldings\tValue ({0})\n".format(self.currency) + colors.ENDC)
         for coin in portfolio:
             for c in coin_data:
                 if c['symbol'] == coin:
                     name = c['symbol']
                     price = c['price_' + self.currency.lower()]
                     market_cap = c['market_cap_' + self.currency.lower()]
-                    percent_change = c['percent_change_1h']
+	            percent_change = c['percent_change_1h']
+                    if percent_change.startswith("-"):
+                        percent_change = colors.FAIL + percent_change + "%" + colors.ENDC
+                    else:
+                        percent_change = colors.OKGREEN + percent_change + "%" + colors.ENDC
                     holdings = portfolio[coin]
-                    value = float(holdings) * float(price)
-                    print("{}\t{}\t\t{}\t\t{}%\t\t{}\t\t{}\n".format(name, price, market_cap, percent_change, holdings, value))
+                    value = float(holdings) * float(price) 
+                    value = colors.OKBLUE + colors.BOLD + str(value) + colors.ENDC
+                    price = colors.HEADER + str(price) + colors.ENDC
+                    print("{}\t{}\t\t{}\t\t{}\t\t{}\t\t{}\n".format(name, price, market_cap, percent_change, holdings, value))
 
 
     def addToPortfolio(self, currency, amount):
